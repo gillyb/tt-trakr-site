58f15646fcf0a89085a243d66557bd8bae6b78f4
gillyb <gillyb@gmail.com>
2014-08-18 00:17:47 +0300

    slowly making some more progress

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,7 +4,8 @@ app.get('/', function(req, res) {
 });
 
 app.get('/download', function(req, res) {
-	
+	var file = __dirname + '/../download/TimeTrakr.zip';
+	res.download(file);
 });
 
 app.get('/buy', function(req, res) {
@@ -12,5 +13,5 @@ app.get('/buy', function(req, res) {
 });
 
 app.get('/support', function(req, res) {
-
+	res.render('support');
 });
